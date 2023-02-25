f980b4297d113db6ef75684810562645ed6b95eb
eversinc33 <svrath@web.de>
2023-02-25 16:51:21 -0500

    add vram stuff

--- memory/Vram.cpp
+++ memory/Vram.cpp
@@ -1,11 +1,55 @@
 #include "Vram.h"
 
-Vram::Vram()
+uint16_t Vram::get_16bit_texel(const uint16_t& x, const uint16_t& y)
 {
-
+    // return texel data at x, y from VRAM
+    int index = (y * VRAM_WIDTH) + x;
+    return this->vram[index];
 }
 
-Vram::~Vram()
+uint16_t Vram::get_8bit_texel(const uint16_t& x, const uint16_t& y)
 {
+	int index = (y * VRAM_WIDTH) + x;
+    uint16_t data = this->vram[index];
 
+    // as 8bit -> two pixels in 16bit store
+	this->vram[index * 2 + 0] = (uint8_t)data;
+	this->vram[index * 2 + 1] = (uint8_t)(data >> 8);
+}
+
+uint16_t Vram::get_4bit_texel(const uint16_t& x, const uint16_t& y)
+{
+	int index = (y * VRAM_WIDTH) + x;
+    uint16_t data = this->vram[index];
+
+    // as 4 bit -> four pixels in 16bit store
+	this->vram[index * 4 + 0] = (uint8_t)data & 0xf;
+	this->vram[index * 4 + 1] = (uint8_t)(data >> 4) & 0xf;
+	this->vram[index * 4 + 2] = (uint8_t)(data >> 8) & 0xf;
+	this->vram[index * 4 + 3] = (uint8_t)(data >> 12) & 0xf;
+}
+    
+void Vram::store_16bit_texel(const uint16_t& x, const uint16_t& y, const uint16_t& data)
+{
+    // as 16bit -> one pixel in 16bit store
+	int index = (y * VRAM_WIDTH) + x;
+    this->vram[index] = data;
+}
+
+void Vram::store_8bit_texels(const uint16_t& x, const uint16_t& y, const uint16_t& data)
+{
+    // as 8bit -> two pixels in 16bit store
+	int index = (y * VRAM_WIDTH) + x;
+	this->vram[index * 2 + 0] = (uint8_t)data;
+	this->vram[index * 2 + 1] = (uint8_t)(data >> 8);
+}
+
+void Vram::store_4bit_texels(const uint16_t& x, const uint16_t& y, const uint16_t& data)
+{
+    // as 4 bit -> four pixels in 16bit store
+	int index = (y * VRAM_WIDTH) + x;
+	this->vram[index * 4 + 0] = (uint8_t)data & 0xf;
+	this->vram[index * 4 + 1] = (uint8_t)(data >> 4) & 0xf;
+	this->vram[index * 4 + 2] = (uint8_t)(data >> 8) & 0xf;
+	this->vram[index * 4 + 3] = (uint8_t)(data >> 12) & 0xf;
 }
--- memory/Vram.h
+++ memory/Vram.h
@@ -4,6 +4,12 @@
 #pragma once
 
 #include <stdint.h>
+#include "../gpu/Gpu.h"
+
+// 1MB of VRAM
+#define VRAM_WIDTH 1024
+#define VRAM_HEIGHT 512
+#define VRAM_SIZE VRAM_WIDTH*VRAM_HEIGHT
 
 class Vram
 {
@@ -11,11 +17,15 @@ public:
     Vram();
     ~Vram();
 
-    uint16_t vram[1024 * 512] = { 0 }; // 1 MB Vram
-
-
+    uint16_t get_4bit_texel(const uint16_t& x, const uint16_t& y);
+    uint16_t get_8bit_texel(const uint16_t& x, const uint16_t& y);
+    uint16_t get_16bit_texel(const uint16_t& x, const uint16_t& y);
+    void store_4bit_texels(const uint16_t& x, const uint16_t& y, const uint16_t& data);
+    void store_8bit_texels(const uint16_t& x, const uint16_t& y, const uint16_t& data);
+    void store_16bit_texel(const uint16_t& x, const uint16_t& y, const uint16_t& data);
+    
 private:
-
+    uint16_t vram[VRAM_SIZE];
 };
 
 #endif
